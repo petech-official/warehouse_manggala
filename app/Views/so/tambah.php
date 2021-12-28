7a4a3cae8ff1be420c57ad266bab8bb22859f7c1
petech-official <dianrosapratama2000@gmail.com>
2021-12-28 09:06:32 +0530

    SO

--- app/Views/so/tambah.php
+++ app/Views/so/tambah.php
@@ -24,20 +24,45 @@
                         <form id="quickForm" method="POST" action="/<?= $judul; ?>/save">
                             <div class="card-body">
                                 <div class="form-group">
-                                    <label for="zzz">zzz</label>
-                                    <input type="text" class="form-control <?= ($validation->hasError('zzz')) ? 'is-invalid' : ''; ?>" id="zzz" name="zzz" autofocus value="<?= old('zzz'); ?>" placeholder="Masukan zzz">
+                                    <label for="noso">No SO</label>
+                                    <input type="text" class="form-control <?= ($validation->hasError('noso')) ? 'is-invalid' : ''; ?>" id="noso" name="noso" autofocus value="<?= old('noso'); ?>" placeholder="Masukan noso">
                                     <div class="invalid-feedback">
-                                        <?= $validation->getError('zzz'); ?>
+                                        <?= $validation->getError('noso'); ?>
                                     </div>
                                 </div>
+                                 <!-- Date dd/mm/yyyy -->
                                 <div class="form-group">
-                                    <label for="zzz">zzz</label>
-                                    <input type="text" class="form-control <?= ($validation->hasError('zzz')) ? 'is-invalid' : ''; ?>" id="zzz" name="zzz" autofocus value="<?= old('zzz'); ?>" placeholder="Masukan zzz">
+                                <label>Date masks:</label>
+                                <div class="input-group">
+                                    <div class="input-group-prepend">
+                                    <span class="input-group-text"><i class="far fa-calendar-alt"></i></span>
+                                    </div>
+                                    <input type="text" class="form-control" data-inputmask-alias="datetime" data-inputmask-inputformat="dd/mm/yyyy" data-mask id="datemask">
+                                </div>
+                                
+                                <div class="form-group">
+                                    <label for="customer">customer</label>
+                                    <select class="form-select form-control  <?= ($validation->hasError('customer')) ? 'is-invalid' : ''; ?>" aria-label="Default select example" autofocus value="<?= old('customer'); ?>" name="customer" id="customer">
+                                        <option selected disabled>Pilih customer</option>
+                                        <?php foreach ($dataCustomer as $key => $value) : ?>
+                                            <option value="<?= $value['id']; ?>"><?= $value['nama']; ?></option>
+                                        <?php endforeach ?>
+                                    </select>
                                     <div class="invalid-feedback">
-                                        <?= $validation->getError('zzz'); ?>
+                                        <?= $validation->getError('customer'); ?>
                                     </div>
                                 </div>
-                            </div>
+
+                                <div class="form-group">
+                                    <label for="alamat_kirim">Alamat Kirim</label>
+                                    <select class="form-select form-control  <?= ($validation->hasError('alamat_kirim')) ? 'is-invalid' : ''; ?>" aria-label="Default select example" autofocus value="<?= old('alamat_kirim'); ?>" name="alamat_kirim" id="alamat_kirim">                                        
+                                    </select>
+                                    <div class="invalid-feedback">
+                                        <?= $validation->getError('alamat_kirim'); ?>
+                                    </div>
+                                </div>
+
+                                
                             <!-- /.card-body -->
                             <div class="card-footer">
                                 <button type="submit" class="btn btn-primary">Submit</button>
@@ -45,10 +70,42 @@
                         </form>
                     </div>
                 </div>
-
             </div>
         </div>
     </div>
 </div>
+<div class="row">
+    <div class="col">
+        <table>
+            <tr>
+                <td></td>            
+            </tr>
+        </table>
+    </div>
+</div>
+<script>
+    $('#customer').change(function() {
+        var customer = $('#customer').val();        
+        $.ajax({
+                url: "/<?= $judul ?>/getAlamat",
+                type: "POST",
+                data: {
+                    namaCustomer: customer
+                },
+                dataType: "json",
+                success: function(data) {
+                    // let obj = JSON.parse(data)
+                    var baris = '';
+                    for (let i = 0; i < data.length; i++) {
+                        const element = data[i];
+                        baris += '<option value="' + element.id + '" >' + element.alamat + '</option>';
+                    }
+                    $('#alamat_kirim').html(baris);
+                }
+            })
+    });
+
+     
+</script>
 
 <?= $this->endSection(); ?>
